df5f1ca9f68ea8bb99923c995332399207874e5b
Adrian Bradu <adrian.bradu@gmail.com>
2023-03-02 13:05:22 +0200

    new base path update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/assets',
+  base: '/KisiFrontendChallenge/',
   test: {
     globals: true,
     environment: 'happy-dom',
